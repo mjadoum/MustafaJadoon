d933c748f39ab1dc83d113adcdf63f3dc2c63fa9
Mustafa-Jadoun <54470645+mjadoum@users.noreply.github.com>
2022-01-07 01:05:49 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,39 @@
+var typed = new Typed(".typing", {
+    strings: ["", "Hello!", "I am", "Mustafa", "Jadoun", "Web", "Designer"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+})
+// overlay with slidshow
+//on
+function on1() {
+    document.getElementById("overlay-1").style.display = "block";
+}
+function on2() {
+    document.getElementById("overlay-2").style.display = "block";
+}
+function on3() {
+    document.getElementById("overlay-3").style.display = "block";
+}
+function on4() {
+    document.getElementById("overlay-4").style.display = "block";
+}
+function on5() {
+    document.getElementById("overlay-5").style.display = "block";
+}
+//off
+function off1() {
+    document.getElementById("overlay-1").style.display = "none";
+}
+function off2() {
+    document.getElementById("overlay-2").style.display = "none";
+}
+function off3() {
+    document.getElementById("overlay-3").style.display = "none";
+}
+function off4() {
+    document.getElementById("overlay-4").style.display = "none";
+}
+function off5() {
+    document.getElementById("overlay-5").style.display = "none";
+}
